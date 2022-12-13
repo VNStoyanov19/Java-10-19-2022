db15b3497410cbb8d6833d54d21fce461873809a
vnstoyanov19 <vnstoyanov19@codingburgas.bg>
2022-12-13 08:45:58 +0200

    Extending subclasses to superclasses

--- VNStoyanov-12-06-2022/src/Mammal.java
+++ VNStoyanov-12-06-2022/src/Mammal.java
@@ -21,14 +21,15 @@ public class Mammal {
     }
 
     public void setAge(int age) {
-        if (age < 0) {
+        if (age > 0) {
             this.age = age;
         }
     }
 
     public int getAge() {
-
-        return age;
+        if (age < 0) {
+            return age;
+        }
     }
 
 }
